775c37e5c1439eacd9e762d34dce702d182cca7a
DrRman <104178011+DrRman25@users.noreply.github.com>
2022-09-15 03:03:16 +0000

    Updated v3 Code Editor help section

--- code-editor/3/help/using-the-editor/script.js
+++ code-editor/3/help/using-the-editor/script.js
@@ -20,3 +20,21 @@ function displayNotification(relativeElement, messageText, notificationTime) {
 document.getElementById("screen-width").addEventListener("click", function(e) {
     displayNotification(e.target, "Width: " + innerWidth + "px", 2000);
 });
+
+var frameDoc1 = document.getElementById("frame-1").contentDocument || document.getElementById("frame-1").contentWindow.document;
+
+frameDoc1.open();
+frameDoc1.write(`<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My HTML</title>
+</head>
+
+<body>
+    <p>Hello, this is my code!</p>
+</body>
+
+</html>
+`);
+frameDoc1.close();
